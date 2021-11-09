76c0c7bee6820d554ee01fc7027ec54556bef11a
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 17:07:39 +0300

    feat: add Sum without highest and lowest number task

--- codewars.js
+++ codewars.js
@@ -32,3 +32,14 @@ function fakeBin(x){
   function check(a, x) {
     return a.includes(x);
 }
+
+// Sum without highest and lowest number
+function sumArray(array) {
+    if (array == null || array.length < 2){
+      return 0;
+    } else {
+    let min = Math.min(...array);
+    let max = Math.max(...array);
+      return array.reduce((a,b) => a + b, 0) - min - max;
+  }
+    }
