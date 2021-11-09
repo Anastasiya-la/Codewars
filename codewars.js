1d7024e35d237dd8101efcaa723a1a0bae130df8
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-09 15:43:12 +0300

    feat: add fake binary task

--- codewars.js
+++ codewars.js
@@ -1,1 +1,15 @@
-
+// Fake Binary
+function fakeBin(x){
+    let arr = x.split('')
+    let newArr = [];
+    for(let i=0; i<arr.length; i++) {
+      if(arr[i]<5) {
+        arr[i] = 0;
+        newArr.push(arr[i])
+      } else {
+        arr[i] = 1;
+        newArr.push(arr[i])
+      }
+    }
+    return newArr.join('');
+  }
