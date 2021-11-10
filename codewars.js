d5ab1cfbfcb2820182e77f49e0813deebf038e03
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-10 14:35:45 +0300

    feat: add Find Maximum and Minimum Values of a List task

--- codewars.js
+++ codewars.js
@@ -146,3 +146,13 @@ function firstNonConsecutive (arr) {
    return null;
  }
 
+ //Find Maximum and Minimum Values of a List
+ var min = function(list){
+    
+  return Math.min(...list);
+}
+
+var max = function(list){
+  
+  return Math.max(...list);
+}
