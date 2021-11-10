d63bb42eeb64bee323d25181fee0d06e9ca1577b
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-10 14:32:57 +0300

    feat: add Find the first non-consecutive number task

--- codewars.js
+++ codewars.js
@@ -136,3 +136,13 @@ function booleanToString(b){
   //your code here
 }
 
+//Find the first non-consecutive number
+function firstNonConsecutive (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+     if (arr[i] + 1 !== arr[i + 1]) {
+       return arr[i + 1]
+     };
+   };
+   return null;
+ }
+
