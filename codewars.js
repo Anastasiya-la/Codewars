651fa2329e70a014d65d688cd121756f9337dcbf
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-11 22:51:49 +0300

    feat: add Cat years, Dog years task

--- codewars.js
+++ codewars.js
@@ -462,3 +462,15 @@ function updateLight(current) {
 function strCount(str, letter){  
   return str.split(letter).length - 1;
 }
+
+//Cat years, Dog years
+var humanYearsCatYearsDogYears = function(humanYears) {
+  switch (humanYears) {
+     case 1:
+       return [1, 15, 15]
+     case 2:
+       return [2, 24, 24];
+     default:
+       return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24 ];
+   }
+ }
