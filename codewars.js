10bfc1f5c0b7e73a3fa72086eb3b8b4b076cfe2e
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-11 19:04:37 +0300

    feat: add Get Planet Name By ID task

--- codewars.js
+++ codewars.js
@@ -412,3 +412,36 @@ const areaOrPerimeter = function(l , w) {
   return l === w ? l * w : (l + w) * 2;
   // Return your answer
 };
+
+//Get Planet Name By ID
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break
+    case 2:
+      name = 'Venus'
+      break
+    case 3:
+      name = 'Earth'
+      break
+    case 4:
+      name = 'Mars'
+      break
+    case 5:
+      name = 'Jupiter'
+      break
+    case 6:
+      name = 'Saturn'
+      break
+    case 7:
+      name = 'Uranus'
+      break
+    case 8:
+      name = 'Neptune'
+      break
+  }
+  
+  return name;
+}
