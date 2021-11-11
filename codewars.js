02946ecd7d22ba2932aeca4c638c5e59a08b38dc
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-11 16:55:20 +0300

    feat: add Grasshopper - Grade book task

--- codewars.js
+++ codewars.js
@@ -365,3 +365,19 @@ function setAlarm(employed, vacation){
 function oddCount(n){
   return Math.floor(n / 2);
   }
+
+//Grasshopper - Grade book
+function getGrade (s1, s2, s3) {
+  let avg = (s1 + s2 + s3) / 3;
+  if (avg < 60){
+    return 'F';
+  } else if (avg < 70){
+    return 'D';
+  } else if (avg < 80){
+    return 'C';
+  } else if (avg < 90){
+    return 'B';
+  } else {
+    return 'A';
+  }
+}
