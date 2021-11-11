ad631eb4a5737baa2a02bb9aab3590fc34097f7a
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-11 19:11:23 +0300

    feat: add Thinkful - Logic Drills: task

--- codewars.js
+++ codewars.js
@@ -445,3 +445,15 @@ function getPlanetName(id){
   
   return name;
 }
+
+//Thinkful - Logic Drills: Traffic light
+function updateLight(current) {
+  switch(current){
+      case 'green':
+      return 'yellow';
+      case 'yellow':
+      return 'red';
+      case 'red':
+      return 'green';
+  }
+}
