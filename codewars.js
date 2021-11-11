5162fb82f594c21e3af0d4ec7935c2b1b889098d
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-11 16:13:50 +0300

    feat: add Transportation on vacation task

--- codewars.js
+++ codewars.js
@@ -330,3 +330,15 @@ function greet (name, owner) {
   return name === owner ? 'Hello boss' : 'Hello guest';
   // Add code here
 }
+
+//Transportation on vacation
+function rentalCarCost(d) {
+  if (d < 3){
+    return d * 40;
+  } else if (d >= 3 && d < 7){
+    return d * 40 - 20;
+  } else if (d >= 7) {
+    return d * 40 - 50;
+  }
+  // Your solution here
+}
