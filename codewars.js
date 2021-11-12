b4b4e870b3c1345cee3a6d847716e3823a932ae8
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-12 23:31:57 +0300

    feat: add  Summation task

--- codewars.js
+++ codewars.js
@@ -765,3 +765,12 @@ function boolToWord(bool){
       return 'No';
   }
 }
+
+// Summation
+let summation = function(num){
+  let sum = 0;
+  for (let i = 0; i <= num; i++){
+      sum = sum + i;
+  }
+  return sum;
+}
