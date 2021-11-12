eb7a7ccece90a98ed14cc2eda7a027b34dde6812
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-12 17:44:23 +0300

    feat: add Keep up the hoop task

--- codewars.js
+++ codewars.js
@@ -631,3 +631,8 @@ function switchItUp(number){
       return 'Nine';
   }
 }
+
+//Keep up the hoop
+function hoopCount (n) {
+  return n < 10 ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+}
