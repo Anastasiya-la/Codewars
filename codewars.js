55058008fced2d346a2288f4d9b31f3178ee3689
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-12 18:16:41 +0300

    feat: add Unfinished Loop - Bug Fixing #1 task

--- codewars.js
+++ codewars.js
@@ -674,3 +674,14 @@ function generateRange(min, max, step){
   }
   return arr;
 }
+
+//Unfinished Loop - Bug Fixing #1
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
