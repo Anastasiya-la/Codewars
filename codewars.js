177eef63acdffc4098570f2d557f70ba49e011b2
Ana Rabetskaya <nastia1362@gmail.com>
2021-11-13 17:32:56 +0300

    feat: add Count of positives / sum of negative task

--- codewars.js
+++ codewars.js
@@ -868,3 +868,21 @@ function maps(x){
 //function maps(x){
 //    return x.map(n => n * 2);
 //}
+
+//Count of positives / sum of negative
+function countPositivesSumNegatives(input) {
+  if (input == null || input.length == 0){
+    return [];
+  }
+    let sumPos = 0;
+    let sumNeg = 0;
+    for (let i = 0; i < input.length;i++){
+      if(input[i] > 0){
+        ++sumPos;
+      } else {
+        sumNeg += input[i];
+      }
+    
+    }
+    return [sumPos, sumNeg];
+}
